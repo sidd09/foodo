78ac272eafb3c67c4631c09ac2a0f4f11edbbac8
arnaringib <arnaringib@8fdffab8-ace1-11de-8c46-7bd654735883>
2009-09-29 11:26:27 +0000

    Changed Activity to MapActivity. -Arnar

--- Foodo/src/is/hi/foodo/FoodoMap.java
+++ Foodo/src/is/hi/foodo/FoodoMap.java
@@ -1,13 +1,18 @@
 package is.hi.foodo;
 
+import com.google.android.maps.MapActivity;
+
 import android.app.Activity;
 import android.os.Bundle;
 
-public class FoodoMap extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+public class FoodoMap extends MapActivity{
+
+	/** Called when the activity is first created. */
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+  
+ 
 }
