000e309bdbec97270e2d473432e9f2d3f4f33eaf
siggijons27 <siggijons27@gmail.com>
2009-09-29 12:14:58 +0000

    Created empty test to test JUnit setup

---
+++ FoodoTests/src/is/hi/FoodoTests/EmptyTest.java
@@ -0,0 +1,9 @@
+package is.hi.FoodoTests;
+
+import junit.framework.TestCase;
+
+public class EmptyTest extends TestCase {
+	public void testNothing() {
+		assertEquals("nothing", "nothing");
+	}
+}
