6b07d2f7c75ab69180819db612ae63d31059e0a2
siggijons27 <siggijons27@gmail.com>
2009-09-30 00:59:22 +0000

    Setup a working mapview and added a sepperate resource file for the maps api key for local development.

--- Foodo/src/is/hi/foodo/FoodoMap.java
+++ Foodo/src/is/hi/foodo/FoodoMap.java
@@ -1,8 +1,25 @@
 package is.hi.foodo;
 
-import com.google.android.maps.MapActivity;
+import android.os.Bundle;
+import android.widget.LinearLayout;
 
-public class FoodoMap extends MapActivity{
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
+
+public class FoodoMap extends MapActivity {
+	
+	LinearLayout linearLayout;
+	MapView mapView;
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setBuiltInZoomControls(true);
+    }
 
 	/** Called when the activity is first created. */
 	@Override
