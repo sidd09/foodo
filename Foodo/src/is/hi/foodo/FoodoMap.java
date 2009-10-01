c86b95daa864f2e386a8a5b85a6cd4e2195bc653
arnaringib <arnaringib@8fdffab8-ace1-11de-8c46-7bd654735883>
2009-10-01 01:57:44 +0000

    Added a new class FoodoOverlays. Also I put one little overlay on the map.

--- Foodo/src/is/hi/foodo/FoodoMap.java
+++ Foodo/src/is/hi/foodo/FoodoMap.java
@@ -1,16 +1,25 @@
 package is.hi.foodo;
 
+import java.util.List;
+
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
 
 
 public class FoodoMap extends MapActivity {
 	
 	LinearLayout linearLayout;
 	MapView mapView;
+	List<Overlay> mapRestaurantsOverlays;
+	Drawable drawable;
+	FoodoOverlays foodoRestaurantsOverlays;
 	
 	@Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,20 @@ public class FoodoMap extends MapActivity {
         
         mapView = (MapView) findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
+        
+        //Overlays
+        mapRestaurantsOverlays = mapView.getOverlays();
+        //Need some new icon
+        drawable = this.getResources().getDrawable(R.drawable.burger_joint);
+        foodoRestaurantsOverlays = new FoodoOverlays(drawable);
+        
+        // Test(Arnar) - Can be commented out!
+        GeoPoint point = new GeoPoint(64139603,-21955812);
+        OverlayItem overlayItem = new OverlayItem(point, "", "");
+        
+        foodoRestaurantsOverlays.addOverlay(overlayItem);
+        mapRestaurantsOverlays.add(foodoRestaurantsOverlays);
+        
     }
 
 	/** Called when the activity is first created. */
