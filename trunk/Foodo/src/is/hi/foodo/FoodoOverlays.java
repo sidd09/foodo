cd0db97a83a43946e222a2f3fcec8ac523b6ff1c
arnaringib <arnaringib@8fdffab8-ace1-11de-8c46-7bd654735883>
2009-10-01 01:57:44 +0000

    Added a new class FoodoOverlays. Also I put one little overlay on the map.

---
+++ trunk/Foodo/src/is/hi/foodo/FoodoOverlays.java
@@ -0,0 +1,41 @@
+package is.hi.foodo;
+
+import java.util.ArrayList;
+
+import android.graphics.drawable.Drawable;
+
+import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.OverlayItem;
+
+public class FoodoOverlays extends ItemizedOverlay {
+	private ArrayList<OverlayItem> mRestaurantsOverlays = new ArrayList<OverlayItem>(); 
+	
+	public FoodoOverlays(Drawable defaultMarker) {
+		super(boundCenter(defaultMarker));
+	}
+	
+	/**
+	 * 
+	 * @param overlay
+	 * Add a new restaurant overlay.
+	 */
+	public void addOverlay(OverlayItem overlay){
+		mRestaurantsOverlays.add(overlay);
+		populate(); //Every overlayitem is read and prepared to be drawned
+	}	
+	
+	@Override
+	protected OverlayItem createItem(int i) {
+		return mRestaurantsOverlays.get(i);
+	}
+
+	@Override
+	/**
+	 * @return size()
+	 * Returns the number of overlays
+	 */
+	public int size() {
+		return mRestaurantsOverlays.size();
+	}
+
+}
