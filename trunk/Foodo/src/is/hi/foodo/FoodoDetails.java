0be234925e41d9073a3ec0f8bf852ce8c7bbbe90
siggijons27@gmail.com <siggijons27@gmail.com@8fdffab8-ace1-11de-8c46-7bd654735883>
2009-10-04 22:16:41 +0000

    Initial setup for the Details activity with a blank layout.

---
+++ trunk/Foodo/src/is/hi/foodo/FoodoDetails.java
@@ -0,0 +1,15 @@
+package is.hi.foodo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FoodoDetails extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.details);
+		
+	}
+
+}
