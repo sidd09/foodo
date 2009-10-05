b74012d6c44f900d4a6a107e571f1fd6bf14e1bb
arnaringib <arnaringib@8fdffab8-ace1-11de-8c46-7bd654735883>
2009-10-05 01:29:41 +0000

    Added clickable Overlay, when pressed overlay you get a Toast with the title of the overlay.

--- trunk/Foodo/src/is/hi/foodo/FoodoOverlays.java
+++ trunk/Foodo/src/is/hi/foodo/FoodoOverlays.java
@@ -3,8 +3,12 @@ package is.hi.foodo;
 import java.util.ArrayList;
 
 import android.graphics.drawable.Drawable;
+import android.view.MotionEvent;
+import android.widget.Toast;
 
+import com.google.android.maps.GeoPoint;
 import com.google.android.maps.ItemizedOverlay;
+import com.google.android.maps.MapView;
 import com.google.android.maps.OverlayItem;
 
 public class FoodoOverlays extends ItemizedOverlay<OverlayItem> {
@@ -21,7 +25,7 @@ public class FoodoOverlays extends ItemizedOverlay<OverlayItem> {
 	 */
 	public void addOverlay(OverlayItem overlay){
 		mRestaurantsOverlays.add(overlay);
-		populate(); //Every overlayitem is read and prepared to be drawned
+		populate(); //Every overlayItem is read and prepared to be drawned
 	}	
 	
 	@Override
@@ -37,5 +41,28 @@ public class FoodoOverlays extends ItemizedOverlay<OverlayItem> {
 	public int size() {
 		return mRestaurantsOverlays.size();
 	}
-
+	
+	@Override
+	public boolean onTouchEvent(MotionEvent event, MapView mapView) {
+		if(event.getAction() == 1)//1 == ACTION_UP 
+		{
+			GeoPoint p  = mapView.getProjection().fromPixels(
+						(int) event.getX(),
+						(int) event.getY());
+			
+			int count = 0;
+			while(count < mRestaurantsOverlays.size()){
+				if((mRestaurantsOverlays.get(count).getPoint().getLatitudeE6() + 1000 >= p.getLatitudeE6() &&
+					mRestaurantsOverlays.get(count).getPoint().getLatitudeE6() - 1000 <= p.getLatitudeE6()) &&
+					(mRestaurantsOverlays.get(count).getPoint().getLongitudeE6() + 1000 >= p.getLongitudeE6() &&
+					mRestaurantsOverlays.get(count).getPoint().getLongitudeE6() - 1000 <= p.getLongitudeE6())){
+					Toast.makeText(mapView.getContext(), 
+						mRestaurantsOverlays.get(count).getTitle(), 
+						Toast.LENGTH_LONG).show();
+				}
+				count++;
+			}
+		}
+		return false;
+	}
 }
