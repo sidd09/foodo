ed7eab808911ad38dc3d2d45006566ce5968c2f8
beggaboo85 <beggaboo85@8fdffab8-ace1-11de-8c46-7bd654735883>
2009-10-07 00:06:27 +0000

    menu in main and detail

--- trunk/Foodo/src/is/hi/foodo/FoodoDetails.java
+++ trunk/Foodo/src/is/hi/foodo/FoodoDetails.java
@@ -1,7 +1,11 @@
 package is.hi.foodo;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class FoodoDetails extends Activity {
 
@@ -11,5 +15,28 @@ public class FoodoDetails extends Activity {
 		setContentView(R.layout.details);
 		
 	}
+	/* create the menu items */
+	@Override 
+	public boolean onCreateOptionsMenu(Menu menu) {
+		 menu.add(0,0,0,"Info");		 
+		 return true;
+	}
+	
+	CharSequence text1 = "In progress";
+	int duration = Toast.LENGTH_SHORT;
+
+	/* when menu button option selected */
+	@Override 
+	public boolean onOptionsItemSelected(MenuItem item) {
+		Context context = getApplicationContext();
+		switch (item.getItemId()) {
+		case 0:
+			Toast toast1 = Toast.makeText(context, text1, duration);
+			toast1.show();
+			return true;
+		}
+		return false;
+	}
+  
 
 }
