e7bee4eb092763b345cef4ebcc9c0fe89779e431
siggijons27@gmail.com <siggijons27@gmail.com@8fdffab8-ace1-11de-8c46-7bd654735883>
2009-10-15 12:14:10 +0000

    renamed packaged to match conventions

---
+++ trunk/FoodoTests/src/is/hi/foodo/tests/EmptyTest.java
@@ -0,0 +1,9 @@
+package is.hi.foodo.tests;
+
+import junit.framework.TestCase;
+
+public class EmptyTest extends TestCase {
+	public void testNothing() {
+		assertEquals("nothing", "nothing");
+	}
+}
