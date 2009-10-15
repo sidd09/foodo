7998a35cc7ab6e75c0898c3a4b0616d3da4eddff
siggijons27@gmail.com <siggijons27@gmail.com@8fdffab8-ace1-11de-8c46-7bd654735883>
2009-10-15 12:14:41 +0000

    Removed old package

--- trunk/FoodoTests/src/is/hi/FoodoTests/EmptyTest.java
+++
@@ -1,9 +0,0 @@
-package is.hi.FoodoTests;
-
-import junit.framework.TestCase;
-
-public class EmptyTest extends TestCase {
-	public void testNothing() {
-		assertEquals("nothing", "nothing");
-	}
-}
