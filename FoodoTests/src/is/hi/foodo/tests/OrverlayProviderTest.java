1347d701cdd0e0c4e7be7930abef7e364c60bf3e
siggijons27 <siggijons27@gmail.com>
2009-10-15 12:14:10 +0000

    renamed packaged to match conventions

---
+++ FoodoTests/src/is/hi/foodo/tests/OrverlayProviderTest.java
@@ -0,0 +1,58 @@
+package is.hi.foodo.tests;
+
+import is.hi.foodo.FoodoOverlayProvider;
+import is.hi.foodo.OverlayProvider;
+import is.hi.foodo.SimpleOverlayProvider;
+
+import java.util.ArrayList;
+
+import junit.framework.TestCase;
+
+import com.google.android.maps.OverlayItem;
+
+public class OrverlayProviderTest extends TestCase {
+	
+	public void testSimpleProvider() {
+		
+		OverlayProvider op;
+		OverlayItem item;
+		ArrayList<OverlayItem> list;
+		
+		op = new SimpleOverlayProvider();
+		list = op.getAllOverlays();
+		
+		assertTrue(list.size() > 0);
+		
+		item = list.get(0);
+		assertEquals(item.getTitle(), "Burger Joint");
+		assertEquals(item.getPoint().getLatitudeE6(), 64139603);
+		assertEquals(item.getPoint().getLongitudeE6(), -21955812);
+		
+		item = list.get(1);
+		assertEquals(item.getTitle(), "Pizza Joint");
+		assertEquals(item.getPoint().getLatitudeE6(), 64135603);
+		assertEquals(item.getPoint().getLongitudeE6(), -21954812);
+	}
+
+	public void testFoodoProvider() {
+		OverlayProvider op;
+		OverlayItem item;
+		ArrayList<OverlayItem> list;
+		
+		op = new FoodoOverlayProvider();
+		list = op.getAllOverlays();
+		
+		assertTrue(list.size() > 0);
+		
+		item = list.get(0);
+		assertEquals(item.getTitle(), "Burger Joint");
+		assertEquals(item.getPoint().getLatitudeE6(), 64139603);
+		assertEquals(item.getPoint().getLongitudeE6(), -21955812);
+		
+		item = list.get(1);
+		assertEquals(item.getTitle(), "Pizza Joint");
+		assertEquals(item.getPoint().getLatitudeE6(), 64135603);
+		assertEquals(item.getPoint().getLongitudeE6(), -21954812);
+		
+	}
+}
