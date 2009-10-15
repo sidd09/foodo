9edcd76a72f57514741af3d6399069fe17fa2151
siggijons27 <siggijons27@gmail.com>
2009-10-15 12:09:35 +0000

    Created empty content provider for restaurant data

---
+++ Foodo/src/is/hi/foodo/Restaurant.java
@@ -0,0 +1,10 @@
+package is.hi.foodo;
+
+public class Restaurant {
+	public String name;
+	public String description;
+	public int rating;
+	public String phone;
+	public int lat;
+	public int lng;
+}
