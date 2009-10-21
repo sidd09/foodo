b1a1a7ec3d87346b183d0d9b3b9b2b2c742a8363
siggijons27@gmail.com <siggijons27@gmail.com@8fdffab8-ace1-11de-8c46-7bd654735883>
2009-10-21 14:38:56 +0000

    Connected list view to details view.

--- trunk/Foodo/src/is/hi/foodo/FoodoList.java
+++ trunk/Foodo/src/is/hi/foodo/FoodoList.java
@@ -3,8 +3,11 @@ package is.hi.foodo;
 import java.util.ArrayList;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
 public class FoodoList extends ListActivity {
@@ -25,17 +28,6 @@ public class FoodoList extends ListActivity {
 		fillData();
 		getListView().setTextFilterEnabled(true);
 		registerForContextMenu(getListView());
-		
-		/*
-		getList();
-		
-		ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this,
-				R.layout.listrow,
-				R.id.restaurant,
-				mList);
-		setListAdapter(listAdapter);
-		getListView().setTextFilterEnabled(true); //Let user search by typing the name.
-		*/
 	}
 	
 	private void fillData() {
@@ -52,50 +44,15 @@ public class FoodoList extends ListActivity {
 		SimpleCursorAdapter restaurants = 
 				new SimpleCursorAdapter(this, R.layout.listrow, mRestaurantCursor, from, to);
 		setListAdapter(restaurants);
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		super.onListItemClick(l, v, position, id);
 		
+		Intent i = new Intent(this, FoodoDetails.class);
+		i.putExtra(RestaurantDbAdapter.KEY_ROWID, id);
+    	startActivity(i);
 	}
-	/*
-	public void getList(){
-		this.mList = new ArrayList<String>();
-		try {
-			JSONObject result = getResponse("http://foodo.siggijons.net/api/restaurants.json");
-			
-			JSONArray list = result.getJSONArray("Restaurants");
-			for (int i = 0; i < list.length(); i++)
-			{
-				JSONObject o = list.getJSONObject(i);
-				mList.add(o.getString("name"));
-			}
-		}
-		catch(Exception e)
-		{
-			
-		}
-	}
-	
-	private JSONObject getResponse(String service_url) {
-		try {
-			URL url = new URL(service_url + "");
-			URLConnection connection = url.openConnection();
-			
-			BufferedReader reader = new BufferedReader( new InputStreamReader(connection.getInputStream()));
-			StringBuilder builder = new StringBuilder();
-			String line;
-			while (( line = reader.readLine()) != null)
-			{
-				builder.append(line);
-			}
-			
-			JSONObject json;
-			
-			json = new JSONObject(builder.toString());
-			return json.getJSONObject("responseData");
-		}
-		catch (Exception e) {
-			//TODO something
-			return null;
-		}
-	}
-	*/
 	
 }
