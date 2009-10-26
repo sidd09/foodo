b23eb1dbb26581210c67b4da6e63976b8aa25191
siggijons27@gmail.com <siggijons27@gmail.com@8fdffab8-ace1-11de-8c46-7bd654735883>
2009-10-26 18:40:09 +0000

    Fixed bug when no restaurants are in the db.

--- trunk/FoodoServer/src/ApiController.php
+++ trunk/FoodoServer/src/ApiController.php
@@ -6,16 +6,17 @@ class ApiController {
 		$db = new RestaurantDb();
 		
 		$items = array();
-		foreach ($db->selectAll() as $item)
-		{	
-			$items[] = $item->toArray();
+		$r = $db->selectAll();
+		
+		if (count($r) > 0)
+		{
+			foreach ($r as $item)
+			{	
+				$items[] = $item->toArray();
+			}
 		}
 		
-		/*
-		 
-		 {"responseData":{"Restaurants":[{"id":1,"name":"Burger Joint","lat":64139078,"lng":-21936757,"rating":"5.0"},{"id":2,"name":"Pizza Joint","lat":64135603,"lng":-21954812,"rating":"4.0"},{"id":3,"name":"Asian Joint","lat":64136603,"lng":-21953812,"rating":"3.0"}]},"responseDetails":"OK","responseStatus":"200"} 
-		 
-		 */
+		
 		$result = array(
 			"responseData" => array("Restaurants" => $items),
 			"responseDetails" => "OK",
