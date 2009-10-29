8c52d28a6a3b0531f4df98afed8e4d66c0ae8faf
siggijons27 <siggijons27@gmail.com>
2009-10-29 17:22:46 +0000

    Fixed update all in webservice

--- FoodoTests/src/is/hi/foodo/tests/RestaurantWebServiceTest.java
+++ FoodoTests/src/is/hi/foodo/tests/RestaurantWebServiceTest.java
@@ -35,24 +35,30 @@ public class RestaurantWebServiceTest extends AndroidTestCase {
     	
     	assertTrue("not able to load from webservice", mService.updateAll());
 		
+    	/*
 		Cursor r = mDb.fetchRestaurant(1);
 		assertEquals(r.getString(r.getColumnIndexOrThrow(RestaurantDbAdapter.KEY_NAME)), "Burger Joint");
 		assertEquals(r.getInt(r.getColumnIndexOrThrow(RestaurantDbAdapter.KEY_LAT)), 64139078);
 		assertEquals(r.getInt(r.getColumnIndexOrThrow(RestaurantDbAdapter.KEY_LNG)), -21955812);
 		assertTrue(new Float(5.0).equals(r.getFloat(r.getColumnIndexOrThrow(RestaurantDbAdapter.KEY_RATING))));
 		r.deactivate();
-
+    	 */
     }
     
     public void testRating() {
     	
-    	mDb.createRestaurant(1, "test", 0, 0, 5.0, 1);
+    	mService.updateAll();
     	
     	long id = 1;
+    	Cursor r = mDb.fetchRestaurant(1);
+    	
+    	float currentRating = r.getFloat(r.getColumnIndexOrThrow(RestaurantDbAdapter.KEY_RATING));
+    	int currentCount = r.getInt(r.getColumnIndexOrThrow(RestaurantDbAdapter.KEY_RATING_COUNT));
+    	    	
     	double newRating = 2.0;
     	double resRating = mService.addRating(id, newRating);
-    	
-    	assertEquals(resRating, 3.5);
+
+    	assertEquals(((currentRating*currentCount)+newRating)/(currentCount+1), resRating);
     	
     }
 
