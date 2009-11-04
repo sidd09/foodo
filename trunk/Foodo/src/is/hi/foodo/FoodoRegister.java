ef1aa566e076b3f86734319962d044e459bbfd53
beggaboo85 <beggaboo85@8fdffab8-ace1-11de-8c46-7bd654735883>
2009-11-04 00:49:29 +0000

    Login and register view

---
+++ trunk/Foodo/src/is/hi/foodo/FoodoRegister.java
@@ -0,0 +1,13 @@
+package is.hi.foodo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FoodoRegister extends Activity {
+	
+	@Override 
+    public void onCreate(Bundle savedInstanceState) { 
+		super.onCreate(savedInstanceState); 
+        setContentView(R.layout.register); 
+	}
+}
