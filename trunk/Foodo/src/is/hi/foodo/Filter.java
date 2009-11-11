af5d32e1e7039334acd82451a8bec798d16df20a
arnaringib <arnaringib@8fdffab8-ace1-11de-8c46-7bd654735883>
2009-11-11 17:08:53 +0000

    Changed the price filter, now low, medium high. Also added a temp image on the price filter buttons.

--- trunk/Foodo/src/is/hi/foodo/Filter.java
+++ trunk/Foodo/src/is/hi/foodo/Filter.java
@@ -4,8 +4,9 @@ public class Filter {
 	public static CharSequence ratingFrom;
 	public static CharSequence ratingTo;
 	public static int radius;
-	public static CharSequence priceFrom;
-	public static CharSequence priceTo;
+	public static boolean lowprice;
+	public static boolean mediumprice;
+	public static boolean highprice;
 	public static CharSequence[] types;
 	public static boolean[] checkedTypes;
 }
