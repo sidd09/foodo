80b96611591580f8e3be6e19e040ee1e0d14a6b4
beggaboo85 <beggaboo85@8fdffab8-ace1-11de-8c46-7bd654735883>
2009-11-16 15:03:41 +0000

    fixed registration email view and return to login on register

--- Foodo/src/is/hi/foodo/FoodoRegister.java
+++ Foodo/src/is/hi/foodo/FoodoRegister.java
@@ -18,7 +18,7 @@ public class FoodoRegister extends Activity implements Runnable {
 	
 	private Button btnReg;
 	static final int PASSWORD_DIALOG = 0, REGISTER_DIALOG = 1;
-	private CharSequence firstName, lastName, email, password, rePassword;
+	private CharSequence firstName, lastName, email_1, email_2, password, rePassword;
 	private FoodoUserManager uManager;
 	
 	private ProgressDialog pd;
@@ -27,8 +27,7 @@ public class FoodoRegister extends Activity implements Runnable {
     public void onCreate(Bundle savedInstanceState) { 
 		super.onCreate(savedInstanceState); 
         setContentView(R.layout.register); 
-        
-       // setup();
+
         getRegistration();
         
         btnReg = (Button)this.findViewById(R.id.register_button);
@@ -37,32 +36,25 @@ public class FoodoRegister extends Activity implements Runnable {
         uManager = new FoodoUserManager();
 	}
 	
-	/*public void setup(){
-		final EditText rFirstName = (EditText) findViewById(R.id.txt_firstname);
-		
-		final EditText rLastName = (EditText) findViewById(R.id.txt_lastname);
-		
-		final EditText rEmail = (EditText) findViewById(R.id.txt_email);
-		
-		final EditText rPassword = (EditText) findViewById(R.id.txt_password);
-		
-	}*/
-		
 	
 	public void getRegistration() {
 		EditText eFirstName = (EditText) findViewById(R.id.txt_firstname);
 		EditText eLastName = (EditText) findViewById(R.id.txt_lastname);
-		EditText eEmail = (EditText) findViewById(R.id.txt_email);
+		EditText eEmail_1 = (EditText) findViewById(R.id.txt_email_1);
+		EditText eEmail_2 = (EditText) findViewById(R.id.txt_email_2);
 		EditText ePassword = (EditText) findViewById(R.id.txt_newPassword);
 		EditText eRePassword = (EditText) findViewById(R.id.txt_rePassword);
 		
 		firstName = eFirstName.getText();
 		lastName = eLastName.getText();
-		email = eEmail.getText();
+		email_1 = eEmail_1.getText();
+		email_2 = eEmail_2.getText();
 		password = ePassword.getText();
 		rePassword = eRePassword.getText();
 		
 	}
+	
+	// Dialogs in case passwords don't match and/or not all fields are filled out
 	protected Dialog onCreateDialog(int id) {
 		switch (id) {
 			case PASSWORD_DIALOG:
@@ -96,8 +88,8 @@ public class FoodoRegister extends Activity implements Runnable {
 		public void onClick(View v)
 		{
 			if (v==btnReg){
-				if(TextUtils.isEmpty(firstName)|| TextUtils.isEmpty(lastName)|| TextUtils.isEmpty(email) || 
-						 TextUtils.isEmpty(password)){
+				if(TextUtils.isEmpty(firstName)|| TextUtils.isEmpty(lastName)|| TextUtils.isEmpty(email_1) || 
+						TextUtils.isEmpty(email_2) || TextUtils.isEmpty(password)){
 					showDialog(REGISTER_DIALOG);
 				}
 				else if (!TextUtils.equals(password, rePassword)) {
@@ -115,7 +107,7 @@ public class FoodoRegister extends Activity implements Runnable {
 
 	@Override
 	public void run() {
-		uManager.signup(firstName.toString(), lastName.toString(),email.toString(), password.toString());
+		uManager.signup(firstName.toString(), lastName.toString(),email_1.toString()+"@"+email_2.toString(), password.toString());
 		handler.sendEmptyMessage(0);
 	}
 	
@@ -123,8 +115,10 @@ public class FoodoRegister extends Activity implements Runnable {
 		@Override
 		public void handleMessage(Message msg) {
 			pd.dismiss();
-			if (uManager.isAuthenticated())
+			if (uManager.isAuthenticated()){
 				Toast.makeText(FoodoRegister.this, "User registered!", Toast.LENGTH_LONG).show();
+				FoodoRegister.this.finish();
+			}
 			else
 				Toast.makeText(FoodoRegister.this, "Registration failed: " + uManager.getError(), Toast.LENGTH_LONG).show();
 		}
