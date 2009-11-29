f52676078d754f8a31b6e01582b0386f917ed14b
beggaboo85 <beggaboo85@8fdffab8-ace1-11de-8c46-7bd654735883>
2009-11-29 17:10:16 +0000

    black fixed Write review button in ReadReview

--- Foodo/src/is/hi/foodo/ReadReviews.java
+++ Foodo/src/is/hi/foodo/ReadReviews.java
@@ -1,5 +1,6 @@
 package is.hi.foodo;
 
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -15,13 +16,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.preference.PreferenceManager;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -40,6 +42,8 @@ public class ReadReviews extends ListActivity implements Runnable {
 	private ReviewWebService mService;
 	private RestaurantDbAdapter mDbHelper;
 	private Long mRowId;	
+	
+	private Button btnWriteReview;
 	
 	//private Cursor mRestaurantCursor;
 	
@@ -60,6 +64,8 @@ public class ReadReviews extends ListActivity implements Runnable {
 		getListView().setTextFilterEnabled(true);
 		getListView().setClickable(false);
 		registerForContextMenu(getListView());
+		
+		setupButtons();
 		
 		//Check if resuming from a saved instance state
         mRowId = (savedInstanceState != null ? savedInstanceState.getLong(RestaurantDbAdapter.KEY_ROWID) : null);
@@ -108,57 +114,37 @@ public class ReadReviews extends ListActivity implements Runnable {
         setListAdapter(adapter);
 	}
 
-	
-	/*
-	public void gatherList() {
-		
-		//Get all rows from database
-		mRestaurantCursor = mDbHelper.fetchAllRestaurants(Filter.ratingFrom,
-				Filter.ratingTo,
-				Filter.lowprice,
-				Filter.mediumprice,
-				Filter.highprice,
-				Filter.checkedTypes,
-				Filter.typesId);
-		startManagingCursor(mRestaurantCursor);
-		
-		
-        //Create an array of fields we want to display
-		String[] from = new String[]{RestaurantDbAdapter.KEY_NAME, RestaurantDbAdapter.KEY_RATING };
-		
-		//Fields we want to bind to
-		int[] to = new int[]{R.id.reviewName, R.id.reviewText};
-        
-        SimpleCursorAdapter adapt = new SimpleCursorAdapter(this, R.layout.listreview, mRestaurantCursor, from, to);
-
-        
-        setListAdapter(adapt);
-	}*/
-	
-	@Override 
-	public boolean onCreateOptionsMenu(Menu menu) {
-		 menu.add(0,0,0,"Write a review");
-		 return true;
+	public void writeReviews(){
+		Intent i = new Intent(this, WriteReviews.class);
+		i.putExtra(RestaurantDbAdapter.KEY_ROWID, mRowId);
+		startActivityForResult(i, 1);
 	}
-	@Override 
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case 0:
-			SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(this);
-			if(settings.getBoolean("access", false)){
-				Intent writer = new Intent(this, WriteReviews.class);
-				writer.putExtra(RestaurantDbAdapter.KEY_ROWID, mRowId);
-				startActivityForResult(writer, 1);
-				return true;
-			}
-			else{
-				Context context = getApplicationContext();
-				Toast.makeText(context, "You have to be signed in", Toast.LENGTH_SHORT).show();
+	public void setupButtons() {
+		this.btnWriteReview = (Button)this.findViewById(R.id.bWriteReview);
+		
+		btnWriteReview.setOnClickListener(new clicker());
+			
+	}
+	// button click listener
+	class clicker implements Button.OnClickListener
+    {     
+
+		public void onClick(View v)
+		{
+			Context context = getApplicationContext();
+			if(v==btnWriteReview){
+				SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(ReadReviews.this);
+				if(settings.getBoolean("access", true)){
+					writeReviews();
+				}
+				else {
+					Toast.makeText(context, "You have to be signed in", Toast.LENGTH_SHORT).show();
+				}
 			}
 		
 		}
-		return false;
-	}
+    }	
+	
 	
 	private void loadReviews() {
 		pd = ProgressDialog.show(ReadReviews.this, "Loading..", "Updating");
